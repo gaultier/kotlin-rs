dce1b721251ed24aff1427a16655a5083a099e40
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-02-18 17:41:19 +0100

    Impl start_*

--- src/lib.rs
+++ src/lib.rs
@@ -15,12 +15,12 @@ pub enum LexTokenKind {
 #[derive(Debug, Eq, PartialEq)]
 pub struct LexToken {
     pub kind: LexTokenKind,
+    start_pos: usize,
     start_line: usize,
     start_column: usize,
-    start_pos: usize,
+    end_pos: usize,
     end_line: usize,
     end_column: usize,
-    end_pos: usize,
 }
 
 pub struct Lexer {
@@ -44,15 +44,21 @@ impl Lexer {
         }
     }
 
-    pub fn new_token_here(&self, kind: LexTokenKind) -> Result<LexToken, String> {
+    pub fn new_token_here(
+        &self,
+        kind: LexTokenKind,
+        start_pos: usize,
+        start_line: usize,
+        start_column: usize,
+    ) -> Result<LexToken, String> {
         Ok(LexToken {
             kind,
             end_pos: self.pos - 1,
-            end_column: self.column - 1,
             end_line: self.line,
-            start_pos: 0,
-            start_column: 0,
-            start_line: 0,
+            end_column: self.column - 1,
+            start_pos,
+            start_line,
+            start_column,
         })
     }
 
@@ -82,21 +88,38 @@ impl Lexer {
 
     pub fn lex(&mut self) -> Result<LexToken, String> {
         dbg!(&self.src);
+        let start_pos = self.pos;
+        let start_line = self.line;
+        let start_column = self.column;
+
         if self.is_at_end() {
-            return self.new_token_here(LexTokenKind::Eof);
+            return self.new_token_here(LexTokenKind::Eof, start_pos, start_line, start_column);
         }
         let c = self.advance();
 
         match c {
-            Some("+") => self.new_token_here(LexTokenKind::Plus),
-            Some("-") => self.new_token_here(LexTokenKind::Minus),
-            Some("/") => self.new_token_here(LexTokenKind::Star),
-            Some("*") => self.new_token_here(LexTokenKind::Slash),
+            Some("+") => {
+                self.new_token_here(LexTokenKind::Plus, start_pos, start_line, start_column)
+            }
+            Some("-") => {
+                self.new_token_here(LexTokenKind::Minus, start_pos, start_line, start_column)
+            }
+            Some("/") => {
+                self.new_token_here(LexTokenKind::Star, start_pos, start_line, start_column)
+            }
+            Some("*") => {
+                self.new_token_here(LexTokenKind::Slash, start_pos, start_line, start_column)
+            }
             Some("=") => {
                 if self.match_char("=") {
-                    self.new_token_here(LexTokenKind::EqualEqual)
+                    self.new_token_here(
+                        LexTokenKind::EqualEqual,
+                        start_pos,
+                        start_line,
+                        start_column,
+                    )
                 } else {
-                    self.new_token_here(LexTokenKind::Equal)
+                    self.new_token_here(LexTokenKind::Equal, start_pos, start_line, start_column)
                 }
             }
             Some(c) => Err(format!("Unknown token `{}`", c)),
