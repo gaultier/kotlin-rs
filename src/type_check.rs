39eaac0ccec193731ded2175589af7874713d9b9
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-03-02 18:16:20 +0100

    Expand types

--- src/type_check.rs
+++ src/type_check.rs
@@ -7,8 +7,12 @@ use std::fmt;
 pub enum Type {
     Bool,
     Int,
+    UInt,
     Long,
-    Void,
+    ULong,
+    Float,
+    Double,
+    Object,
 }
 
 impl fmt::Display for Type {
@@ -16,8 +20,12 @@ impl fmt::Display for Type {
         match self {
             Type::Bool => write!(f, "Bool"),
             Type::Int => write!(f, "Int"),
+            Type::UInt => write!(f, "UInt"),
             Type::Long => write!(f, "Long"),
-            Type::Void => write!(f, "Void"),
+            Type::ULong => write!(f, "ULong"),
+            Type::Float => write!(f, "Float"),
+            Type::Double => write!(f, "Double"),
+            Type::Object => write!(f, "Object"),
         }
     }
 }
@@ -26,20 +34,15 @@ impl Type {
     fn coalesce(&self, other: &Type, location: &Location) -> Result<Type, Error> {
         match (self, other) {
             (Type::Bool, Type::Bool) => Ok(Type::Bool),
-            (Type::Bool, _) | (_, Type::Bool) => Err(Error::new(
-                ErrorKind::IncompatibleTypes(self.clone(), other.clone()),
-                location.start_pos,
-                location.start_line,
-                location.start_column,
-                location.end_pos,
-                location.end_line,
-                location.end_column,
-            )),
             (Type::Int, Type::Int) => Ok(Type::Int),
+            (Type::UInt, Type::UInt) => Ok(Type::UInt),
             (Type::Long, Type::Long) | (Type::Long, Type::Int) | (Type::Int, Type::Long) => {
                 Ok(Type::Long)
             }
-            (_, Type::Void) | (Type::Void, _) => Err(Error::new(
+            (Type::UInt, Type::ULong) | (Type::ULong, Type::UInt) | (Type::ULong, Type::ULong) => {
+                Ok(Type::ULong)
+            }
+            | _ => Err(Error::new(
                 ErrorKind::IncompatibleTypes(self.clone(), other.clone()),
                 location.start_pos,
                 location.start_line,
