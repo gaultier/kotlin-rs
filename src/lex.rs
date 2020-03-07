b86e0d3e23a7ef8998935fe72c7e63eb298ea219
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-03-07 19:48:32 +0100

    Adapt number()

--- src/lex.rs
+++ src/lex.rs
@@ -7,9 +7,11 @@ use log::debug;
 // use std::result::Result;
 
 #[derive(Debug, Eq, PartialEq, Clone)]
-pub enum NumberType {
-    Integer,
-    Real,
+enum NumberBase {
+    Binary,
+    Octal, // Actuallty invalid but useful for nice error messages
+    Hexadecimal,
+    Decimal,
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -156,6 +158,17 @@ struct CursorToken {
     pub len: usize,
 }
 
+enum CursorTokenKindNumber {
+    Int {
+        base: NumberBase,
+        empty_int: bool,
+    },
+    Float {
+        base: NumberBase,
+        empty_exponent: bool,
+    },
+}
+
 // #[derive(Debug, PartialEq, Clone)]
 // pub struct OwnedToken<'a> {
 //     pub token: &'a Token,
@@ -265,15 +278,13 @@ impl Cursor<'_> {
             // c if is_id_start(c) => self.ident(),
 
             // Numeric literal.
-            // c @ '0'..='9' => {
-            //     let literal_kind = self.number(c);
-            //     let suffix_start = self.len_consumed();
-            //     self.eat_literal_suffix();
-            //     TokenKind::Literal {
-            //         kind: literal_kind,
-            //         suffix_start,
-            //     }
-            // }
+            c @ '0'..='9' => {
+                let literal_kind = self.number(c);
+                let suffix_start = self.len_consumed();
+                self.eat_literal_suffix();
+                let num = 0;
+                TokenKind::Int(num)
+            }
 
             // One-symbol tokens.
             ';' => TokenKind::Semicolon,
@@ -383,91 +394,91 @@ impl Cursor<'_> {
     //     Ident
     // }
 
-    // fn number(&mut self, first_digit: char) -> LiteralKind {
-    //     debug_assert!('0' <= self.prev() && self.prev() <= '9');
-    //     let mut base = Base::Decimal;
-    //     if first_digit == '0' {
-    //         // Attempt to parse encoding base.
-    //         let has_digits = match self.first() {
-    //             'b' => {
-    //                 base = Base::Binary;
-    //                 self.bump();
-    //                 self.eat_decimal_digits()
-    //             }
-    //             'o' => {
-    //                 base = Base::Octal;
-    //                 self.bump();
-    //                 self.eat_decimal_digits()
-    //             }
-    //             'x' => {
-    //                 base = Base::Hexadecimal;
-    //                 self.bump();
-    //                 self.eat_hexadecimal_digits()
-    //             }
-    //             // Not a base prefix.
-    //             '0'..='9' | '_' | '.' | 'e' | 'E' => {
-    //                 self.eat_decimal_digits();
-    //                 true
-    //             }
-    //             // Just a 0.
-    //             _ => {
-    //                 return Int {
-    //                     base,
-    //                     empty_int: false,
-    //                 }
-    //             }
-    //         };
-    //         // Base prefix was provided, but there were no digits
-    //         // after it, e.g. "0x".
-    //         if !has_digits {
-    //             return Int {
-    //                 base,
-    //                 empty_int: true,
-    //             };
-    //         }
-    //     } else {
-    //         // No base prefix, parse number in the usual way.
-    //         self.eat_decimal_digits();
-    //     };
+    fn number(&mut self, first_digit: char) -> CursorTokenKindNumber {
+        debug_assert!('0' <= self.prev() && self.prev() <= '9');
+        let mut base = NumberBase::Decimal;
+        if first_digit == '0' {
+            // Attempt to parse encoding base.
+            let has_digits = match self.first() {
+                'b' => {
+                    base = NumberBase::Binary;
+                    self.bump();
+                    self.eat_decimal_digits()
+                }
+                'o' => {
+                    base = NumberBase::Octal;
+                    self.bump();
+                    self.eat_decimal_digits()
+                }
+                'x' => {
+                    base = NumberBase::Hexadecimal;
+                    self.bump();
+                    self.eat_hexadecimal_digits()
+                }
+                // Not a base prefix.
+                '0'..='9' | '_' | '.' | 'e' | 'E' => {
+                    self.eat_decimal_digits();
+                    true
+                }
+                // Just a 0.
+                _ => {
+                    return CursorTokenKindNumber::Int {
+                        base,
+                        empty_int: false,
+                    }
+                }
+            };
+            // NumberBase prefix was provided, but there were no digits
+            // after it, e.g. "0x".
+            if !has_digits {
+                return CursorTokenKindNumber::Int {
+                    base,
+                    empty_int: true,
+                };
+            }
+        } else {
+            // No base prefix, parse number in the usual way.
+            self.eat_decimal_digits();
+        };
 
-    //     match self.first() {
-    //         // Don't be greedy if this is actually an
-    //         // integer literal followed by field/method access or a range pattern
-    //         // (`0..2` and `12.foo()`)
-    //         '.' if self.second() != '.' && !is_id_start(self.second()) => {
-    //             // might have stuff after the ., and if it does, it needs to start
-    //             // with a number
-    //             self.bump();
-    //             let mut empty_exponent = false;
-    //             if self.first().is_digit(10) {
-    //                 self.eat_decimal_digits();
-    //                 match self.first() {
-    //                     'e' | 'E' => {
-    //                         self.bump();
-    //                         empty_exponent = !self.eat_float_exponent();
-    //                     }
-    //                     _ => (),
-    //                 }
-    //             }
-    //             Float {
-    //                 base,
-    //                 empty_exponent,
-    //             }
-    //         }
-    //         'e' | 'E' => {
-    //             self.bump();
-    //             let empty_exponent = !self.eat_float_exponent();
-    //             Float {
-    //                 base,
-    //                 empty_exponent,
-    //             }
-    //         }
-    //         _ => Int {
-    //             base,
-    //             empty_int: false,
-    //         },
-    //     }
-    // }
+        match self.first() {
+            // Don't be greedy if this is actually an
+            // integer literal followed by field/method access or a range pattern
+            // (`0..2` and `12.foo()`)
+            '.' if self.second() != '.' && !is_id_start(self.second()) => {
+                // might have stuff after the ., and if it does, it needs to start
+                // with a number
+                self.bump();
+                let mut empty_exponent = false;
+                if self.first().is_digit(10) {
+                    self.eat_decimal_digits();
+                    match self.first() {
+                        'e' | 'E' => {
+                            self.bump();
+                            empty_exponent = !self.eat_float_exponent();
+                        }
+                        _ => (),
+                    }
+                }
+                CursorTokenKindNumber::Float {
+                    base,
+                    empty_exponent,
+                }
+            }
+            'e' | 'E' => {
+                self.bump();
+                let empty_exponent = !self.eat_float_exponent();
+                CursorTokenKindNumber::Float {
+                    base,
+                    empty_exponent,
+                }
+            }
+            _ => CursorTokenKindNumber::Int {
+                base,
+                empty_int: false,
+            },
+        }
+    }
 
     // fn lifetime_or_char(&mut self) -> TokenKind {
     //     debug_assert!(self.prev() == '\'');
@@ -668,7 +679,7 @@ impl Cursor<'_> {
         self.eat_decimal_digits()
     }
 
-    // Eats the suffix of the literal, e.g. "_u8".
+    // Eats the suffix of the literal, e.g. "_UL".
     fn eat_literal_suffix(&mut self) {
         self.eat_identifier();
     }
