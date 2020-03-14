56766d328077c0891234b9f162ee6baeca429e09
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-03-14 18:09:30 +0100

    Test error

--- tests/logic.rs
+++ tests/logic.rs
@@ -1,4 +1,5 @@
 use kotlin::compile::compile;
+use kotlin::error::*;
 
 #[test]
 fn int_equality() {
@@ -58,37 +59,85 @@ fn long_equality() {
 }
 
 #[test]
-fn int_long_equality() {
+fn int_long_equality() -> Result<(), &'static str> {
     let src = String::from("0xab == 171L;");
     let mut out: Vec<u8> = Vec::new();
 
-    assert!(compile(src, &mut out).is_err());
-    // assert_eq!(std::str::from_utf8(&out).as_ref().unwrap(), &"171==171;\n");
+    match compile(src, &mut out) {
+        Err(Error {
+            kind: ErrorKind::IncompatibleTypes(..),
+            location:
+                Location {
+                    start_line: 1,
+                    start_column: 6,
+                    end_line: 1,
+                    end_column: 8,
+                    ..
+                },
+        }) => Ok(()),
+        _ => Err("Should be a type error"),
+    }
 }
 
 #[test]
-fn int_uint_long_equality() {
+fn int_uint_long_equality() -> Result<(), &'static str> {
     let src = String::from("0xab == 171UL;");
     let mut out: Vec<u8> = Vec::new();
 
-    assert!(compile(src, &mut out).is_err());
-    // assert_eq!(std::str::from_utf8(&out).as_ref().unwrap(), &"171==171;\n");
+    match compile(src, &mut out) {
+        Err(Error {
+            kind: ErrorKind::IncompatibleTypes(..),
+            location:
+                Location {
+                    start_line: 1,
+                    start_column: 6,
+                    end_line: 1,
+                    end_column: 8,
+                    ..
+                },
+        }) => Ok(()),
+        _ => Err("Should be a type error"),
+    }
 }
 
 #[test]
-fn int_uint_equality() {
+fn int_uint_equality() -> Result<(), &'static str> {
     let src = String::from("0xab == 171U;");
     let mut out: Vec<u8> = Vec::new();
 
-    assert!(compile(src, &mut out).is_err());
-    // assert_eq!(std::str::from_utf8(&out).as_ref().unwrap(), &"171==171;\n");
+    match compile(src, &mut out) {
+        Err(Error {
+            kind: ErrorKind::IncompatibleTypes(..),
+            location:
+                Location {
+                    start_line: 1,
+                    start_column: 6,
+                    end_line: 1,
+                    end_column: 8,
+                    ..
+                },
+        }) => Ok(()),
+        _ => Err("Should be a type error"),
+    }
 }
 
 #[test]
-fn float_double_equality() {
+fn float_double_equality() -> Result<(), &'static str> {
     let src = String::from("1f == 1.0;");
     let mut out: Vec<u8> = Vec::new();
 
-    assert!(compile(src, &mut out).is_err());
-    // assert_eq!(std::str::from_utf8(&out).as_ref().unwrap(), &"171==171;\n");
+    match compile(src, &mut out) {
+        Err(Error {
+            kind: ErrorKind::IncompatibleTypes(..),
+            location:
+                Location {
+                    start_line: 1,
+                    start_column: 4,
+                    end_line: 1,
+                    end_column: 6,
+                    ..
+                },
+        }) => Ok(()),
+        _ => Err("Should be a type error"),
+    }
 }
