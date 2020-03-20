5bf85b28ed2e9708d4b87c7b59b0fe35328a8a74
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-03-20 16:13:42 +0100

    Fix if test

--- tests/if.rs
+++ tests/if.rs
@@ -5,8 +5,9 @@ fn simple_if_expr() {
     let src = String::from("if (1<2) 'o' else 'x';");
     let mut out: Vec<u8> = Vec::new();
 
-    let res = compile(src, &mut out);
-    dbg!(&res);
-    assert!(res.is_ok());
-    assert_eq!(std::str::from_utf8(&out).as_ref().unwrap(), &"171===171;\n");
+    assert!(compile(src, &mut out).is_ok());
+    assert_eq!(
+        std::str::from_utf8(&out).as_ref().unwrap(),
+        &"((1<2)?('o'):('x'));\n"
+    );
 }
