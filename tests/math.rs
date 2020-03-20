c7ff53d7ca48e7fe73903613c1fe66265b0fc425
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-03-20 09:24:07 +0100

    Re-activate string tests

--- tests/math.rs
+++ tests/math.rs
@@ -1,6 +1,6 @@
 use kotlin::compile::compile;
-// use kotlin::error::*;
-// use kotlin::parse::Type;
+use kotlin::error::*;
+use kotlin::parse::Type;
 
 #[test]
 fn add_int_int() {
@@ -131,208 +131,208 @@ fn complex_math() {
     );
 }
 
-// #[test]
-// fn add_string_string() {
-//     let src = String::from(r##""abc" + "def";"##);
-//     let mut out: Vec<u8> = Vec::new();
+#[test]
+fn add_string_string() {
+    let src = String::from(r##""abc" + "def";"##);
+    let mut out: Vec<u8> = Vec::new();
 
-//     assert!(compile(src, &mut out).is_ok());
-//     assert_eq!(
-//         std::str::from_utf8(&out).as_ref().unwrap(),
-//         &r##""abc"+"def";
-// "##
-//     );
-// }
+    assert!(compile(src, &mut out).is_ok());
+    assert_eq!(
+        std::str::from_utf8(&out).as_ref().unwrap(),
+        &r##""abc"+"def";
+"##
+    );
+}
 
-// #[test]
-// fn add_string_int() {
-//     let src = String::from(r##""abc" + 2;"##);
-//     let mut out: Vec<u8> = Vec::new();
+#[test]
+fn add_string_int() {
+    let src = String::from(r##""abc" + 2;"##);
+    let mut out: Vec<u8> = Vec::new();
 
-//     assert!(compile(src, &mut out).is_ok());
-//     assert_eq!(
-//         std::str::from_utf8(&out).as_ref().unwrap(),
-//         &r##""abc"+2;
-// "##
-//     );
-// }
+    assert!(compile(src, &mut out).is_ok());
+    assert_eq!(
+        std::str::from_utf8(&out).as_ref().unwrap(),
+        &r##""abc"+2;
+"##
+    );
+}
 
-// #[test]
-// fn add_int_string() {
-//     let src = String::from(r##"2 + "abc";"##);
-//     let mut out: Vec<u8> = Vec::new();
+#[test]
+fn add_int_string() {
+    let src = String::from(r##"2 + "abc";"##);
+    let mut out: Vec<u8> = Vec::new();
 
-//     assert!(compile(src, &mut out).is_ok());
-//     assert_eq!(
-//         std::str::from_utf8(&out).as_ref().unwrap(),
-//         &r##"2+"abc";
-// "##
-//     );
-// }
+    assert!(compile(src, &mut out).is_ok());
+    assert_eq!(
+        std::str::from_utf8(&out).as_ref().unwrap(),
+        &r##"2+"abc";
+"##
+    );
+}
 
-// #[test]
-// fn add_string_long() {
-//     let src = String::from(r##""abc" + 2L;"##);
-//     let mut out: Vec<u8> = Vec::new();
+#[test]
+fn add_string_long() {
+    let src = String::from(r##""abc" + 2L;"##);
+    let mut out: Vec<u8> = Vec::new();
 
-//     assert!(compile(src, &mut out).is_ok());
-//     assert_eq!(
-//         std::str::from_utf8(&out).as_ref().unwrap(),
-//         &r##""abc"+2;
-// "##
-//     );
-// }
+    assert!(compile(src, &mut out).is_ok());
+    assert_eq!(
+        std::str::from_utf8(&out).as_ref().unwrap(),
+        &r##""abc"+2;
+"##
+    );
+}
 
-// #[test]
-// fn add_long_string() {
-//     let src = String::from(r##"2L + "abc";"##);
-//     let mut out: Vec<u8> = Vec::new();
+#[test]
+fn add_long_string() {
+    let src = String::from(r##"2L + "abc";"##);
+    let mut out: Vec<u8> = Vec::new();
 
-//     assert!(compile(src, &mut out).is_ok());
-//     assert_eq!(
-//         std::str::from_utf8(&out).as_ref().unwrap(),
-//         &r##"2+"abc";
-// "##
-//     );
-// }
+    assert!(compile(src, &mut out).is_ok());
+    assert_eq!(
+        std::str::from_utf8(&out).as_ref().unwrap(),
+        &r##"2+"abc";
+"##
+    );
+}
 
-// #[test]
-// fn add_string_uint() {
-//     let src = String::from(r##""abc" + 2U;"##);
-//     let mut out: Vec<u8> = Vec::new();
+#[test]
+fn add_string_uint() {
+    let src = String::from(r##""abc" + 2U;"##);
+    let mut out: Vec<u8> = Vec::new();
 
-//     assert!(compile(src, &mut out).is_ok());
-//     assert_eq!(
-//         std::str::from_utf8(&out).as_ref().unwrap(),
-//         &r##""abc"+2;
-// "##
-//     );
-// }
+    assert!(compile(src, &mut out).is_ok());
+    assert_eq!(
+        std::str::from_utf8(&out).as_ref().unwrap(),
+        &r##""abc"+2;
+"##
+    );
+}
 
-// #[test]
-// fn add_uint_string() {
-//     let src = String::from(r##"2U + "abc";"##);
-//     let mut out: Vec<u8> = Vec::new();
+#[test]
+fn add_uint_string() {
+    let src = String::from(r##"2U + "abc";"##);
+    let mut out: Vec<u8> = Vec::new();
 
-//     assert!(compile(src, &mut out).is_ok());
-//     assert_eq!(
-//         std::str::from_utf8(&out).as_ref().unwrap(),
-//         &r##"2+"abc";
-// "##
-//     );
-// }
+    assert!(compile(src, &mut out).is_ok());
+    assert_eq!(
+        std::str::from_utf8(&out).as_ref().unwrap(),
+        &r##"2+"abc";
+"##
+    );
+}
 
-// #[test]
-// fn add_string_ulong() {
-//     let src = String::from(r##""abc" + 2UL;"##);
-//     let mut out: Vec<u8> = Vec::new();
+#[test]
+fn add_string_ulong() {
+    let src = String::from(r##""abc" + 2UL;"##);
+    let mut out: Vec<u8> = Vec::new();
 
-//     assert!(compile(src, &mut out).is_ok());
-//     assert_eq!(
-//         std::str::from_utf8(&out).as_ref().unwrap(),
-//         &r##""abc"+2;
-// "##
-//     );
-// }
+    assert!(compile(src, &mut out).is_ok());
+    assert_eq!(
+        std::str::from_utf8(&out).as_ref().unwrap(),
+        &r##""abc"+2;
+"##
+    );
+}
 
-// #[test]
-// fn add_ulong_string() {
-//     let src = String::from(r##"2UL + "abc";"##);
-//     let mut out: Vec<u8> = Vec::new();
+#[test]
+fn add_ulong_string() {
+    let src = String::from(r##"2UL + "abc";"##);
+    let mut out: Vec<u8> = Vec::new();
 
-//     assert!(compile(src, &mut out).is_ok());
-//     assert_eq!(
-//         std::str::from_utf8(&out).as_ref().unwrap(),
-//         &r##"2+"abc";
-// "##
-//     );
-// }
+    assert!(compile(src, &mut out).is_ok());
+    assert_eq!(
+        std::str::from_utf8(&out).as_ref().unwrap(),
+        &r##"2+"abc";
+"##
+    );
+}
 
-// #[test]
-// fn add_string_float() {
-//     let src = String::from(r##""abc" + 2f;"##);
-//     let mut out: Vec<u8> = Vec::new();
+#[test]
+fn add_string_float() {
+    let src = String::from(r##""abc" + 2f;"##);
+    let mut out: Vec<u8> = Vec::new();
 
-//     assert!(compile(src, &mut out).is_ok());
-//     assert_eq!(
-//         std::str::from_utf8(&out).as_ref().unwrap(),
-//         &r##""abc"+2;
-// "##
-//     );
-// }
+    assert!(compile(src, &mut out).is_ok());
+    assert_eq!(
+        std::str::from_utf8(&out).as_ref().unwrap(),
+        &r##""abc"+2;
+"##
+    );
+}
 
-// #[test]
-// fn add_string_double() {
-//     let src = String::from(r##""abc" + 2.0;"##);
-//     let mut out: Vec<u8> = Vec::new();
+#[test]
+fn add_string_double() {
+    let src = String::from(r##""abc" + 2.0;"##);
+    let mut out: Vec<u8> = Vec::new();
 
-//     assert!(compile(src, &mut out).is_ok());
-//     assert_eq!(
-//         std::str::from_utf8(&out).as_ref().unwrap(),
-//         &r##""abc"+2;
-// "##
-//     );
-// }
+    assert!(compile(src, &mut out).is_ok());
+    assert_eq!(
+        std::str::from_utf8(&out).as_ref().unwrap(),
+        &r##""abc"+2;
+"##
+    );
+}
 
-// #[test]
-// fn add_string_bool() {
-//     let src = String::from(r##""abc" + true;"##);
-//     let mut out: Vec<u8> = Vec::new();
+#[test]
+fn add_string_bool() {
+    let src = String::from(r##""abc" + true;"##);
+    let mut out: Vec<u8> = Vec::new();
 
-//     assert!(compile(src, &mut out).is_ok());
-//     assert_eq!(
-//         std::str::from_utf8(&out).as_ref().unwrap(),
-//         &r##""abc"+true;
-// "##
-//     );
-// }
+    assert!(compile(src, &mut out).is_ok());
+    assert_eq!(
+        std::str::from_utf8(&out).as_ref().unwrap(),
+        &r##""abc"+true;
+"##
+    );
+}
 
-// #[test]
-// fn add_bool_string() -> Result<(), String> {
-//     let src = String::from(r##"true + "abc";"##);
-//     let mut out: Vec<u8> = Vec::new();
+#[test]
+fn add_bool_string() -> Result<(), String> {
+    let src = String::from(r##"true + "abc";"##);
+    let mut out: Vec<u8> = Vec::new();
 
-//     match compile(src, &mut out) {
-//         Err(Error {
-//             kind: ErrorKind::IncompatibleTypes(Type::Bool, Type::TString),
-//             location:
-//                 Location {
-//                     start_line: 1,
-//                     start_column: 6,
-//                     end_line: 1,
-//                     end_column: 7,
-//                     ..
-//                 },
-//         }) => Ok(()),
-//         _ => Err("Should fail type checking".to_string()),
-//     }
-// }
+    match compile(src, &mut out) {
+        Err(Error {
+            kind: ErrorKind::IncompatibleTypes(Type::Bool, Type::TString),
+            location:
+                Location {
+                    start_line: 1,
+                    start_column: 6,
+                    end_line: 1,
+                    end_column: 7,
+                    ..
+                },
+        }) => Ok(()),
+        _ => Err("Should fail type checking".to_string()),
+    }
+}
 
-// #[test]
-// fn add_string_null() {
-//     let src = String::from(r##""abc" + null;"##);
-//     let mut out: Vec<u8> = Vec::new();
+#[test]
+fn add_string_null() {
+    let src = String::from(r##""abc" + null;"##);
+    let mut out: Vec<u8> = Vec::new();
 
-//     assert!(compile(src, &mut out).is_ok());
-//     assert_eq!(
-//         std::str::from_utf8(&out).as_ref().unwrap(),
-//         &r##""abc"+null;
-// "##
-//     );
-// }
+    assert!(compile(src, &mut out).is_ok());
+    assert_eq!(
+        std::str::from_utf8(&out).as_ref().unwrap(),
+        &r##""abc"+null;
+"##
+    );
+}
 
-// #[test]
-// fn add_null_string() {
-//     let src = String::from(r##"null + "abc";"##);
-//     let mut out: Vec<u8> = Vec::new();
+#[test]
+fn add_null_string() {
+    let src = String::from(r##"null + "abc";"##);
+    let mut out: Vec<u8> = Vec::new();
 
-//     assert!(compile(src, &mut out).is_ok());
-//     assert_eq!(
-//         std::str::from_utf8(&out).as_ref().unwrap(),
-//         &r##"null+"abc";
-// "##
-//     );
-// }
+    assert!(compile(src, &mut out).is_ok());
+    assert_eq!(
+        std::str::from_utf8(&out).as_ref().unwrap(),
+        &r##"null+"abc";
+"##
+    );
+}
 
 #[test]
 fn add_null_null() {
