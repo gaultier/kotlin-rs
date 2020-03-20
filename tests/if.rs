cc4f8de97cdc93960f858bf24d7821980953733c
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-03-20 12:17:11 +0100

    Add if-expr test

---
+++ tests/if.rs
@@ -0,0 +1,10 @@
+use kotlin::compile::compile;
+
+#[test]
+fn simple_if_expr() {
+    let src = String::from("if (1<2) 'o' else 'x';");
+    let mut out: Vec<u8> = Vec::new();
+
+    assert!(compile(src, &mut out).is_ok());
+    assert_eq!(std::str::from_utf8(&out).as_ref().unwrap(), &"171===171;\n");
+}
