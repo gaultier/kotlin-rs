065bdbf0c9988af7159fd8eb40b38ae974e7fc0a
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-03-22 16:45:10 +0100

    Add range tests

---
+++ tests/range.rs
@@ -0,0 +1,37 @@
+use kotlin::compile::compile;
+use kotlin::error::*;
+use kotlin::parse::Type;
+
+#[test]
+fn simple_range() {
+    let src = String::from("1..5");
+    let mut out: Vec<u8> = Vec::new();
+
+    assert!(compile(src, &mut out).is_ok());
+    assert_eq!(
+        std::str::from_utf8(&out).as_ref().unwrap(),
+        &"(range 1 5)\n"
+    );
+}
+
+#[test]
+fn check_both_types_match() -> Result<(), String> {
+    let src = String::from("1U..5UL");
+    let mut out: Vec<u8> = Vec::new();
+
+    match compile(src, &mut out) {
+        Err(Error {
+            kind: ErrorKind::IncompatibleTypes(Type::UInt, Type::ULong),
+            location:
+                Location {
+                    start_pos: 2,
+                    start_line: 1,
+                    start_column: 3,
+                    end_pos: 4,
+                    end_line: 1,
+                    end_column: 5,
+                },
+        }) => Ok(()),
+        other => Err(format!("Should be a type error: {:?}", other)),
+    }
+}
