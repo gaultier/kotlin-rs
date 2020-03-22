44cf84940843a09dcf4d39b475ebeb484be72365
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-03-22 21:03:08 +0100

    s/begin/do

--- src/sexp_emitter.rs
+++ src/sexp_emitter.rs
@@ -231,7 +231,7 @@ impl SexpEmitter<'_> {
 
     fn block<W: std::io::Write>(&self, ast: &[AstNodeStmt], w: &mut W) -> Result<(), Error> {
         if ast.len() != 1 {
-            write!(w, "(begin ").unwrap();
+            write!(w, "(do ").unwrap();
         }
 
         // Akin to ast.map(...).join(" ")
