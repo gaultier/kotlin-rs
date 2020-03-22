208ed5de2aee0416646b6d2131d85404b107e220
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-03-22 21:12:44 +0100

    Add while tests

---
+++ tests/while.rs
@@ -0,0 +1,27 @@
+use kotlin::compile::compile;
+use kotlin::error::*;
+use kotlin::parse::Type;
+
+#[test]
+fn while_with_body() {
+    let src = String::from("while (1 < 10) { 'c' }");
+    let mut out: Vec<u8> = Vec::new();
+
+    assert!(compile(src, &mut out).is_ok());
+    assert_eq!(
+        std::str::from_utf8(&out).as_ref().unwrap(),
+        &"(while (< 1 10) 'c')\n\n"
+    );
+}
+
+#[test]
+fn while_without_body() {
+    let src = String::from("while (1 < 10) ;");
+    let mut out: Vec<u8> = Vec::new();
+
+    assert!(compile(src, &mut out).is_ok());
+    assert_eq!(
+        std::str::from_utf8(&out).as_ref().unwrap(),
+        &"(while (< 1 10) )\n\n"
+    );
+}
