093697bdd48678e98c07b83717f58b39ccea5566
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-03-23 20:02:30 +0100

    Add skeleton for resolver

--- src/compile.rs
+++ src/compile.rs
@@ -1,6 +1,7 @@
 use crate::error::*;
 use crate::lex::Lexer;
 use crate::parse::Parser;
+use crate::resolver::Resolver;
 use crate::sexp_emitter::SexpEmitter;
 use crate::type_check::TypeChecker;
 use std::io;
@@ -9,6 +10,9 @@ pub fn compile<W: io::Write>(src: String, w: &mut W) -> Result<(), Error> {
     let mut lexer = Lexer::new(src);
     let mut parser = Parser::new(&mut lexer);
     let mut stmts = parser.parse()?;
+
+    let mut resolver = Resolver::new(&lexer);
+    let resolution = resolver.statements(&stmts)?;
 
     let type_checker = TypeChecker::new(&lexer);
     type_checker.statements(&mut stmts)?;
---
+++ src/resolver.rs
@@ -0,0 +1,23 @@
+use crate::error::*;
+use crate::lex::{Lexer, Token, TokenKind};
+use crate::parse::*;
+
+pub(crate) struct Resolver<'a> {
+    lexer: &'a Lexer,
+    resolution: Resolution,
+}
+
+pub(crate) struct Resolution {}
+
+impl Resolver<'_> {
+    pub(crate) fn new(lexer: &Lexer) -> Resolver {
+        Resolver {
+            lexer,
+            resolution: Resolution {},
+        }
+    }
+
+    pub(crate) fn statements(&mut self, statements: &[AstNodeStmt]) -> Result<Resolution, Error> {
+        unimplemented!()
+    }
+}
