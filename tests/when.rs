daa99d9b58a9eeb57607c50a5f2ddc22aa9e1477
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-03-24 11:55:32 +0100

    Simplify case in tests

--- tests/when.rs
+++ tests/when.rs
@@ -55,7 +55,7 @@ fn when_with_subject() {
     assert!(compile(src, &mut out).is_ok());
     assert_eq!(
         std::str::from_utf8(&out).as_ref().unwrap(),
-        &"(case 5 ((1) 2) ((2) 4) ((3) 6) ((4) 8) ((5) 10) :else 42)\n"
+        &"(case 5 1 2 2 4 3 6 4 8 5 10 :else 42)\n"
     );
 }
 
