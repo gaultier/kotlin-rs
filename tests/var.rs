e7e4132bc942ff27c0542a79ff9a4f2b7554308a
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-03-24 17:19:38 +0100

    Ass assign tests

--- tests/var.rs
+++ tests/var.rs
@@ -94,3 +94,27 @@ fn ref_val() {
         &"(def a 1)(def b (* a 2))(if (< b 4) (do ) (do ))\n"
     )
 }
+
+#[test]
+fn simple_var_assign() {
+    let src = String::from("var a = 1; a = 4;");
+    let mut out: Vec<u8> = Vec::new();
+
+    assert!(compile(src, &mut out).is_ok());
+    assert_eq!(
+        std::str::from_utf8(&out).as_ref().unwrap(),
+        &"(def a 1)(set! a 4)\n\n"
+    );
+}
+
+#[test]
+fn var_assign_with_math_expr() {
+    let src = String::from("var a = 5*10; a = a * 2;\n\n");
+    let mut out: Vec<u8> = Vec::new();
+
+    assert!(compile(src, &mut out).is_ok());
+    assert_eq!(
+        std::str::from_utf8(&out).as_ref().unwrap(),
+        &"(def a (* 5 10))(set! a (* a 2))\n\n"
+    );
+}
