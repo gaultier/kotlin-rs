9df922db487aea1e0b19a696921351e735d50ae4
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-03-24 11:29:26 +0100

    Add var ref test

--- tests/var_decl.rs
+++ tests/var_decl.rs
@@ -35,3 +35,15 @@ fn var_with_no_expr() -> Result<(), String> {
         other => Err(format!("Should be a parse error: {:?}", other)),
     }
 }
+
+#[test]
+fn ref_var() {
+    let src = String::from("var a = 1; var b = a * 2; if(b<4) {} else {}");
+    let mut out: Vec<u8> = Vec::new();
+
+    assert!(compile(src, &mut out).is_ok());
+    assert_eq!(
+        std::str::from_utf8(&out).as_ref().unwrap(),
+        &"(def a 1)(def b (* a 2))(if (< b 4) (do ) (do ))\n"
+    )
+}
