1922b121731833da5826ac980bcb2c837805ec3a
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-03-26 14:14:08 +0100

    Add fn test

--- tests/fn.rs
+++ tests/fn.rs
@@ -5,12 +5,25 @@ use kotlin::compile::compile;
 #[test]
 fn simple_call() {
     // FIXME
-    let src = String::from("val a = 1; a();");
+    let src = String::from("fun a() = 10; a();");
     let mut out: Vec<u8> = Vec::new();
 
     assert!(compile(src, &mut out).is_ok());
     assert_eq!(
         std::str::from_utf8(&out).as_ref().unwrap(),
-        &"(def a 1)\n(apply a '())\n"
+        &"(defn a [] 10)\n(apply a '())\n"
+    );
+}
+
+#[test]
+fn assign_to_call_expr() {
+    // FIXME
+    let src = String::from("var a = 1; fun foo() = 99; a = foo();");
+    let mut out: Vec<u8> = Vec::new();
+
+    assert!(compile(src, &mut out).is_ok());
+    assert_eq!(
+        std::str::from_utf8(&out).as_ref().unwrap(),
+        &"(def a 1)\n(defn foo [] 99)\n(set! a (apply foo '()))\n\n"
     );
 }
