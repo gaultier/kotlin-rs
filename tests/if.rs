c8b3ab9f725c92ea74485a9e25b612c9f1b14255
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-03-27 13:47:42 +0100

    --wip-- [skip ci]

--- tests/if.rs
+++ tests/if.rs
@@ -10,7 +10,7 @@ fn simple_if_expr() {
     assert!(compile(src, &mut out).is_ok());
     assert_eq!(
         std::str::from_utf8(&out).as_ref().unwrap(),
-        &"(if (< 1 2) 'o'  'x' )\n"
+        &"(begin (if (< 1 2) 'o'  'x' ) )\n"
     );
 }
 
