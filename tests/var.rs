3d5639474475af7112ff6ad3b4eb192960ebecbd
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-03-27 14:02:43 +0100

    --wip-- [skip ci]

--- tests/var.rs
+++ tests/var.rs
@@ -47,7 +47,7 @@ fn ref_var() {
     assert!(compile(src, &mut out).is_ok());
     assert_eq!(
         std::str::from_utf8(&out).as_mut().unwrap().trim(),
-        "(define a 1)\n(define b (* a 2))\n(if (< b 4) (begin ) (begin )"
+        "(begin (define a 1)\n (define b (* a 2))\n (if (< b 4) (begin ) (begin )) )"
     )
 }
 
@@ -97,7 +97,7 @@ fn ref_val() {
     assert!(compile(src, &mut out).is_ok());
     assert_eq!(
         std::str::from_utf8(&out).as_mut().unwrap().trim(),
-        "(define a 1)\n(define b (* a 2))\n(if (< b 4) (begin ) (begin )"
+        "(begin (define a 1)\n (define b (* a 2))\n (if (< b 4) (begin ) (begin )) )"
     )
 }
 
@@ -109,7 +109,7 @@ fn simple_var_assign() {
     assert!(compile(src, &mut out).is_ok());
     assert_eq!(
         std::str::from_utf8(&out).as_mut().unwrap().trim(),
-        "(define a 1)\n(set! a 4)"
+        "(begin (define a 1)\n (set! a 4)\n )"
     );
 }
 
