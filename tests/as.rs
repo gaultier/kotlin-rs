711869fcbdf302316e484d33d009574a6c637884
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-04-01 11:56:26 +0200

    Add as test

---
+++ tests/as.rs
@@ -0,0 +1,13 @@
+use kotlin::compile::compile;
+
+#[test]
+fn as_expr() {
+    let src = String::from("val a: Int = 4 as Int");
+    let mut out: Vec<u8> = Vec::new();
+
+    assert!(compile(src, &mut out).is_ok());
+    assert_eq!(
+        std::str::from_utf8(&out).as_mut().unwrap().trim(),
+        "(define a (as 4 Int))"
+    );
+}
