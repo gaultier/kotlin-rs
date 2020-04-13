19ba377c8dc49168e6e684e766e557bddaced684
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-04-13 11:29:27 +0200

    Add equality of doubles

--- src/jvm_emitter.rs
+++ src/jvm_emitter.rs
@@ -397,7 +397,7 @@ impl CodeBuilder {
             OP_IF_ICMPNE | OP_IFEQ | OP_IFNE => {
                 self.stack_pop()?;
             }
-            OP_LCMP => {
+            OP_LCMP | OP_DCMPL => {
                 self.stack_pop2()?;
             }
             OP_GOTO => {}
@@ -868,6 +868,13 @@ impl<'a> JvmEmitter<'a> {
                     }
                     (TokenKind::EqualEqual, Type::Float, Type::Float) => {
                         code_builder.push1(OP_FCMPL)?;
+                        code_builder.push3(OP_IFNE, 0x00, 0x07, Type::Int)?;
+                        code_builder.push1(OP_ICONST_1)?;
+                        code_builder.push3(OP_GOTO, 0x00, 0x04, Type::Int)?;
+                        code_builder.push1(OP_ICONST_0)
+                    }
+                    (TokenKind::EqualEqual, Type::Double, Type::Double) => {
+                        code_builder.push1(OP_DCMPL)?;
                         code_builder.push3(OP_IFNE, 0x00, 0x07, Type::Int)?;
                         code_builder.push1(OP_ICONST_1)?;
                         code_builder.push3(OP_GOTO, 0x00, 0x04, Type::Int)?;
