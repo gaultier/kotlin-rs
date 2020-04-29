b1d6c250266227a87b7573f8ea1e902b3898e80c
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-04-29 13:25:14 +0200

    Add Registers

--- src/asm_registers.rs
+++ src/asm_registers.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 // x86_64 specific
 #[derive(Debug, PartialEq, Eq)]
 enum Register {
@@ -39,6 +41,29 @@ impl From<u16> for Register {
             16384 => Register::R14,
             32768 => Register::R15,
             _ => unreachable!(),
+        }
+    }
+}
+
+impl fmt::Display for Register {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Register::Rax => f.write_str("rax"),
+            Register::Rbx => f.write_str("rbx"),
+            Register::Rcx => f.write_str("rcx"),
+            Register::Rdx => f.write_str("rdx"),
+            Register::Rbp => f.write_str("rbp"),
+            Register::Rsp => f.write_str("rsp"),
+            Register::Rsi => f.write_str("rsi"),
+            Register::Rdi => f.write_str("rdi"),
+            Register::R8 => f.write_str("r8"),
+            Register::R9 => f.write_str("r9"),
+            Register::R10 => f.write_str("r10"),
+            Register::R11 => f.write_str("r11"),
+            Register::R12 => f.write_str("r12"),
+            Register::R13 => f.write_str("r13"),
+            Register::R14 => f.write_str("r14"),
+            Register::R15 => f.write_str("r15"),
         }
     }
 }
