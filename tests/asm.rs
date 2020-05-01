ec0e9c91199491918592c11033a70ff73710396f
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-05-01 20:10:24 +0200

    Add int var test

--- tests/asm.rs
+++ tests/asm.rs
@@ -216,3 +216,11 @@ fn long_if_false() {
     let output = asm(src, &path).unwrap().unwrap().stdout;
     assert_eq!(String::from_utf8_lossy(&output).trim(), "-10");
 }
+
+#[test]
+fn int_var() {
+    let src = "var a = 2; println(a)";
+    let path = Path::new("IntVarAsm.kts");
+    let output = asm(src, &path).unwrap().unwrap().stdout;
+    assert_eq!(String::from_utf8_lossy(&output).trim(), "2");
+}
