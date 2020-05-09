95fe0d40eaedaa8b91366e48a9a52b6bec2db1f8
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-05-09 21:56:22 +0200

    fix ret code

--- test.sh
+++ test.sh
@@ -7,12 +7,15 @@ DIFF_TOOL="${DIFF_TOOL:-diff}"
 BIN="${BIN:-../target/debug/kotlin}"
 
 cd e2e
+
+res=0
 for f in *.kts
 do
     if ! ${DIFF_TOOL} \
         <(awk -F '// Expect: ' '/Expect/{print $2}' "$f") \
         <(${BIN} -f "$f" jvm); then
             printf "ERR\tjvm\t$f\n"
+            res=1
     else
         printf "OK\tjvm\t$f\n"
     fi
@@ -21,9 +24,12 @@ do
         <(awk -F '// Expect: ' '/Expect/{print $2}' "$f") \
         <(${BIN} -f "$f" asm); then
             printf "ERR\tasm\t$f\n"
+            res=1
     else
         printf "OK\tasm\t$f\n"
     fi
 done
 
 cd ..
+
+exit $res
