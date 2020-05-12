50df405395465f9802263590fd4e31cfc31b168c
Philippe Gaultier <philippe.gaultier@ppro.com>
2020-05-12 18:58:47 +0200

    Add test

---
+++ e2e/assign_if_any.kts
@@ -0,0 +1,2 @@
+val x: Any = if (1<2) 99 as Any else 42 // Expect: 
+val z: Any = 3; val y: Any = if (1<2) z else 42 // Expect: 
--- tests/if.rs
+++ tests/if.rs
@@ -25,30 +25,6 @@ fn check_both_branches_types_match() -> Result<(), String> {
 }
 
 #[test]
-fn check_both_branches_types_match_unit_when_empty_else() {
-    let src = "if (1<2) 'a' else {} \n";
-    let mut out: Vec<u8> = Vec::new();
-
-    assert!(sexp(src, &mut out).is_ok());
-    assert_eq!(
-        std::str::from_utf8(&out).as_mut().unwrap().trim(),
-        "(if (< 1 2) 'a' (begin ))"
-    );
-}
-
-#[test]
-fn check_both_branches_types_match_unit_when_empty_if() {
-    let src = "if (1<2) ; else 42 \n";
-    let mut out: Vec<u8> = Vec::new();
-
-    assert!(sexp(src, &mut out).is_ok());
-    assert_eq!(
-        std::str::from_utf8(&out).as_mut().unwrap().trim(),
-        "(if (< 1 2) (begin ) 42)"
-    );
-}
-
-#[test]
 fn check_types_coalesce() -> Result<(), String> {
     let src = "if (1<2) 99 else 99f \n";
     let mut out: Vec<u8> = Vec::new();
@@ -60,28 +36,4 @@ fn check_types_coalesce() -> Result<(), String> {
         }) => Ok(()),
         other => Err(format!("Should be a type error: {:?}", other)),
     }
-}
-
-#[test]
-fn any() {
-    let src = "val x: Any = if (1<2) 99 as Any else 42 \n";
-    let mut out: Vec<u8> = Vec::new();
-
-    assert!(sexp(src, &mut out).is_ok());
-    assert_eq!(
-        std::str::from_utf8(&out).as_mut().unwrap().trim(),
-        "(define x (if (< 1 2) (as 99 Any) 42))"
-    );
-}
-
-#[test]
-fn any_2() {
-    let src = "val x: Any = 3; val y: Any = if (1<2) x else 42 \n";
-    let mut out: Vec<u8> = Vec::new();
-
-    assert!(sexp(src, &mut out).is_ok());
-    assert_eq!(
-        std::str::from_utf8(&out).as_mut().unwrap().trim(),
-        "(begin (define x 3)\n (define y (if (< 1 2) x 42))\n )"
-    );
 }
